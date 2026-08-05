f93eb8598e62789f2024cded630f495a0ed60da6
Yahia <ismailhatab88@gmail.com>
2026-08-05 20:00:52 +0300

    fix: add missing Link import in StudioBanner to fix TypeScript build error

--- components/common/StudioBanner.tsx
+++ components/common/StudioBanner.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ExternalLink, Sparkles, Dumbbell, Globe, Layers } from "lucide-react";
 
